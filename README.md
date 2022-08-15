f4ad22cbaf5b85df3705b2577f886bb10786351a
Tarun Sukhani <tsukhani@gmail.com>
2022-08-15 11:26:48 +0800

    Spring Boot files

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # b40-api
+
 B40 API
 
-
 ### This is a hello from the backend team
+
 ## A simple project using Java
---
+++ src/test/java/com/pinelabs/b40api/B40ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinelabs.b40api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B40ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
