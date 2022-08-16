6b8f3460997d7fd18ba8e5bd0183c16b43b92f33
Muaz Wazir <muazwazir@gmail.com>
2022-08-16 23:41:10 +0800

    Bean for ModelMapper

--- src/main/java/com/pinelabs/b40api/B40ApiApplication.java
+++ src/main/java/com/pinelabs/b40api/B40ApiApplication.java
@@ -1,13 +1,20 @@
 package com.pinelabs.b40api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class B40ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(B40ApiApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
     }
 
 }
